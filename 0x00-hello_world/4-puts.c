0da35cb94f7a545928f3f1e793107aa2d71d76f2
kbenjilalietu <k.benjilali_etu@enset-media.ac.ma>
2023-10-06 05:17:22 -0700

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,10 @@
 #include <stdio.h>
+/*
+ * main - Entry point
+ *
+ * Description: Outputs a string to stdout using puts
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
