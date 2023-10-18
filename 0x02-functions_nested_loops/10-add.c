f7ae1ce597044da7db17e9c86b30ab693905682e
kbenjilalietu <k.benjilali_etu@enset-media.ac.ma>
2023-10-18 09:22:26 -0700

    10. a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,10 @@
+/**
+ * add - adds two integers and returns the result.
+ * @n and @m : represente 2 integers
+ *
+ * Return: the adds of n and m
+ */
+int add(int n, int m)
+{
+	return (n + m);
+}
