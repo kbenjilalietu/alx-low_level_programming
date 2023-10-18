97d076e64997cb11e8d7317fc6c23fe265b35ced
kbenjilalietu <k.benjilali_etu@enset-media.ac.ma>
2023-10-18 10:08:17 -0700

    11. 98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,7 +7,7 @@
  */
 void print_to_98(int n)
 {
-	if(n <= 98)
+	if (n <= 98)
 	{
 		for (; n < 98; n++)
 		{
@@ -22,5 +22,5 @@ void print_to_98(int n)
 			printf("%d, ", n);
 		}
 		printf("98\n");
-    }
+	}
 }
