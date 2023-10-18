0889fea0e42f31c8dc2b08da864098006829a75b
kbenjilalietu <k.benjilali_etu@enset-media.ac.ma>
2023-10-18 09:54:57 -0700

    11. 98 Battery Street, the OG

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_to_98 - prints all natural numbers from n to 98, followed by \n.
+ * @n: The first printed number
+ *
+ */
+void print_to_98(int n)
+{
+    if (n <= 98)
+    {
+        for (; n < 98; n++)
+        {
+            printf("%d, ", n);
+        }
+        printf("98\n");
+    }
+    else
+    {
+        for (; n > 98; n--)
+        {
+            printf("%d, ", n);
+        }
+        printf("98\n");
+    }
+}
