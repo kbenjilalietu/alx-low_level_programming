551503af6086f4f7402031bacd3659c5807835ad
kbenjilalietu <k.benjilali_etu@enset-media.ac.ma>
2023-10-19 07:19:35 -0700

    15. Even Liber Abbaci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the first 50 Fibonacci numbers,
+ *        starting with 1 and 2, followed by a new line.
+ * Return: Always 0.
+ */
+int main(void)
+{
+	unsigned long int j, k, next, sum;
+
+	j = 1;
+	k = 2;
+	sum = 0;
+	next = 0;
+	while(next <= 4000000)
+	{
+		next = j + k;
+		j = k;
+		k = next;
+		if (next % 2 == 0)
+			sum = sum + next;
+
+	}
+	printf("%lu\n", sum);
+	return (0);
+}
