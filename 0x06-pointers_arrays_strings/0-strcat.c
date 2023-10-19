cb694673f1ef7b0e04db8ea7d217813841df29f7
kbenjilalietu <k.benjilali_etu@enset-media.ac.ma>
2023-10-19 13:49:07 -0700

    0. strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,22 +9,21 @@
  */
 char *_strcat(char *dest, char *src)
 {
-    char *original_dest = dest; /* Save the original pointer to return later */
-
-    /* Move to the end of dest */
-    while (*dest)
-        dest++;
-
-    /* Copy src to dest */
-    while (*src)
-    {
-        *dest = *src;
-        dest++;
-        src++;
-    }
-
-    /* Add the terminating null byte */
-    *dest = '\0';
-
-    return (original_dest);
+	char *original_dest = dest; /* Save the original pointer to return later */
+	
+	/* Move to the end of dest */
+	while (*dest)
+		dest++;
+	
+	/* Copy src to dest */
+	while (*src)
+	{
+		*dest = *src;
+		dest++;
+		src++
+	}
+	
+	/* Add the terminating null byte */
+	*dest = '\0';
+	return (original_dest);
 }
